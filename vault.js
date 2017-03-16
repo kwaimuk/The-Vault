d2893c6eb0c53b9cd119c4fb0541342cc17364ba
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-03-15 18:36:08 -1000

    cleaned up and ready for review

--- vault.js
+++ vault.js
@@ -1,16 +1,20 @@
 'use strict';
 
 function vault() {
-var vault ={};
-var passcode ={};
+//create two objects for storage. Put in records for vault and passcode.
+var vault = {};
+var passcode = {};
 
   function _setValue(key,value){
+    //incoming key will assign to passcode
     passcode=key;
+    //incoming value will assing to vault
     vault=value;
   }
 
   function _getValue(key){
-    if(key===passcode){
+    //if incoming key is matched to current passcode, or not, return null
+    if(key === passcode){
         return vault;
       }else{
         return null;
@@ -20,6 +24,8 @@ var passcode ={};
 
 
 	return {
+    /*for npm test looking for gV and sV, it will
+    look for _gV and _sV which are functions*/
 		getValue: _getValue,
 		setValue:_setValue
 	};
